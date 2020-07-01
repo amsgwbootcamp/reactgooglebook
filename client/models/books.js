a96c0d71650f00d122a1447dec0fa20b3b102727
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-06-30 20:34:51 -0400

    Created mongoose Book Schema

---
+++ client/models/books.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  
+  title: {
+    type: String,
+    trim: true,
+    required: "Book TItle is Required"
+  },
+
+  authors: {
+    type: [String]
+  },
+
+  description: {
+    type: String,
+    trim: true
+  },
+
+  image: {
+    type: String,
+    trim: true
+  },
+
+  link: {
+    type: String,
+    trim: true,
+  }
+
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;

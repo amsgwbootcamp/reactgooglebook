955f005441e0eee4ad8914888d97915c3fc5e697
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-07-03 10:47:35 -0400

    Formatting for search has been completed

--- client/src/components/BooksList/index.js
+++ client/src/components/BooksList/index.js
@@ -1,43 +1,47 @@
 import React from 'react';
+import "./style.css";
 
 // Separate the UI specific transforming logic to utils folder
 //import { bookAuthors } from '../utils';
 const bookAuthors = (authors) => {
-    if (authors) {
-        if (authors.length <= 2) {
-            authors = authors.join(" and ");
-        } else if (authors.length > 2) {
-            let lastAuthor = " and " + authors.slice(-1);
-            authors.pop();
-            authors = authors.join(", ");
-            authors += lastAuthor;
-        }
-    } else {
-        authors = "Author not provided";
+  if (authors) {
+    if (authors.length <= 2) {
+      authors = authors.join(" and ");
+    } else if (authors.length > 2) {
+      let lastAuthor = " and " + authors.slice(-1);
+      authors.pop();
+      authors = authors.join(", ");
+      authors += lastAuthor;
     }
-    return authors;
+  } else {
+    authors = "Author not provided";
+  }
+  return authors;
 }
 
 const Book = ({ book }) => {
-    
   return (
-    <li>
-      <div>
-        <img
-          alt={`${book.volumeInfo.title} book`}
-          src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-        />
-        <div>
-          <h3>{book.volumeInfo.title}</h3>
-          <p>{bookAuthors(book.volumeInfo.authors)}</p>
-          <p>{book.volumeInfo.publishedDate}</p>
-          <p>{book.volumeInfo.description}</p>
-          <p>{book.volumeInfo.infoLink}</p>
-          <p>{book.id}</p>
-        </div>
+    <div>
+      <div className="row" key={book.id}>
+          <div className="col">{book.volumeInfo.title}
+            <button onClick={() => book.saveBook(book.id)} className="save">Save</button>
+            <button onClick={() => book.viewBook(book.id)} className="view">View</button>
+          </div>
       </div>
-      <hr />
-    </li>
+      <div className="row">  
+        <div className="col">{book.volumeInfo.publishedDate}</div>
+      </div>
+      <div className="row">  
+        <div className="col">{bookAuthors(book.volumeInfo.authors)}</div>
+      </div>  
+      <div className="row">
+        <div className="col img-fluid" >
+          <img alt={`${book.volumeInfo.title} book`}
+           src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
+          <span className="text">{book.volumeInfo.description}</span> 
+        </div>
+      </div>  
+      </div>
   );
 };
 

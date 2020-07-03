7b79c4d2f3cf446467cf2891285ec9a771ee841e
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-07-02 20:06:15 -0400

    Search works but handles errors yet

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
-import "./App.css";
 import Nav from "./components/Nav";
+
+import "./App.css";
 
 function App() {
   return (

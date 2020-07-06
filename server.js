6edde57bef9c60ae2a53fe5e26d8a29f546c62dc
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-07-06 14:48:29 -0400

    Removed extra files

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 Book.create()
   .then(dbBook => {

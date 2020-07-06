ef8023a660c5a302365b486a9f73376a1db22d0c
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-07-06 18:38:30 -0400

    Modified code

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,7 +3,7 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/googlebooks"
+  "mongodb://user:password1@ds027761.mlab.com:27761/heroku_6kd0j3x6"
 );
 const bookSeed = [
   {
--- server.js
+++ server.js
@@ -22,12 +22,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
-
 // Send every other request to the React app
 // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 Book.create()
   .then(dbBook => {
